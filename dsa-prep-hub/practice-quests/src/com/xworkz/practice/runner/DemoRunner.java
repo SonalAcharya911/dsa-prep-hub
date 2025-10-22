2e5c395ad44c57a21db702e0a3ae70c70321967f
SonalAcharya911 <sonalacharya911@gmail.com>
2025-10-23 00:27:51 +0530

    demo test

---
+++ dsa-prep-hub/practice-quests/src/com/xworkz/practice/runner/DemoRunner.java
@@ -0,0 +1,33 @@
+
+package com.xworkz.practice.runner;
+
+public class DemoRunner {
+    static {
+        System.out.println("running static block");
+    }
+
+    public DemoRunner(){
+        System.out.println("created DemoRunner");
+    }
+    public static void main(String[] args) {
+        DemoRunner demoRunner=new DemoRunner();
+        demoRunner.product();
+        sum();
+    }
+
+    public static void sum(){
+        System.out.println("running sum");
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("sum="+sum);
+    }
+
+    public void product(){
+        System.out.println("running product");
+        DemoRunner.sum();
+    }
+    static {
+        System.out.println("running another static block");
+    }
+}
